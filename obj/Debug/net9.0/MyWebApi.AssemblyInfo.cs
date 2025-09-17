b2ccd9e910985c0c9ddd7aa1f1e5a890060b8166
Johnny Trinh <johnny.trinh@hotmail.se>
2025-09-17 17:17:04 +0200

    Update AssemblyInformationalVersion

--- obj/Debug/net9.0/MyWebApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a60334d75c31802ee998ae8fdaac4a5a6b75f82")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
